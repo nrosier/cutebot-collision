39a748d2a0d3fd7ce67c7bf34a097911fdb03081
Nick Rosier <github@bunbun.be>
2020-12-29 15:35:56 +0100

    Fix loop code

--- main.ts
+++ main.ts
@@ -6,10 +6,10 @@ basic.forever(function () {
         cuteBot.motors(0, 0)
         for (let index = 0; index < 4; index++) {
             music.playTone(880, music.beat(BeatFraction.Quarter))
-            basic.pause(2000)
-            cuteBot.motors(randint(-50, -100), 0)
-            basic.pause(500)
         }
+        basic.pause(2000)
+        cuteBot.motors(randint(-50, -100), 0)
+        basic.pause(500)
     } else {
         cuteBot.forward()
     }
