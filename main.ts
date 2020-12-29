53903a17d12ebfd47e468d77400afea478f4faff
Nick Rosier <github@bunbun.be>
2020-12-29 15:26:05 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let sonar = 0
+cuteBot.forward()
+basic.forever(function () {
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (sonar > 2 && sonar < 15) {
+        cuteBot.motors(0, 0)
+        for (let index = 0; index < 4; index++) {
+            music.playTone(880, music.beat(BeatFraction.Quarter))
+            basic.pause(2000)
+            cuteBot.motors(randint(-50, -100), 0)
+            basic.pause(500)
+        }
+    } else {
+        cuteBot.forward()
+    }
+})
